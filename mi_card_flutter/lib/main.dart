2aa526dd6734fc128347d40577a3f6a0b70df30d
ryotogashi <ryo.togashi.ca@gmail.com>
2019-05-16 10:34:09 -0700

    ädd dicee-flutter project

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -5,6 +5,84 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/ryo_togashi.jpg'),
+            ),
+            Text(
+              'Ryo Togashi',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'Web & Mobile Developer',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontSize: 15.0,
+                color: Colors.teal.shade100,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 150,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                  size: 20,
+                ),
+                title: Text(
+                  '+1 (778) 697 0270',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                  size: 20,
+                ),
+                title: Text(
+                  'ryo.togashi.ca@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
